5a37bbb8baccd86283138959fc2a128df0bc20bb
rafe <rafemurr@gmail.com>
2025-06-26 22:52:26 -0400

    chore: remove debugging code

--- circuitGraph.h
+++ circuitGraph.h
@@ -1,60 +1,46 @@
 #include "branch.h"
 #include "graph.h"
 #include "math/expression.h"
+#include "math/expressionCostFunction.h"
 #include "math/expressionNode.h"
 #include "resistor.h"
 #include "voltageSource.h"
 #include <ostream>
 #include <stdexcept>
 
+// TODO: refactor this and graph.h to be the same file and class
 class CircuitGraph {
 public:
   CircuitGraph(Graph<Expression, Branch*>& graph);
   Expression getErrorExpression();
   void solveCircuit() {
     ceres::Problem problem;
+    cout << "Current expressions: " << endl;
     for (Expression node : graph.getVertices()) {
       Expression netCurrent = getNodeCurrents(node);
-      netCurrent.print();
       vector<double*> unknowns = netCurrent.getUnknownVals();
-      ceres::CostFunction* costFunction =
-          netCurrent.getCostFunction(unknowns.size());
-      double* x0 = new double[unknowns.size()];
-      cout << "Starting error for this node is: "
-           << netCurrent.root->compute(x0, *netCurrent.getMap()) << endl;
+      ceres::CostFunction* costFunction = netCurrent.getCostFunction();
       problem.AddResidualBlock(costFunction, nullptr, unknowns);
+      cout << netCurrent << endl;
     }
+    cout << "Constraint expressions: " << endl;
     for (Branch* branch : graph.getEdges()) {
       Expression constraint = branch->constraint();
-      constraint.print();
       vector<double*> unknowns = constraint.getUnknownVals();
-      ceres::CostFunction* costFunction =
-          constraint.getCostFunction(unknowns.size());
-      double* x0 = new double[unknowns.size()];
-      cout << "Starting error for this branch is: "
-           << constraint.root->compute(x0, *constraint.getMap()) << endl;
+      ceres::CostFunction* costFunction = constraint.getCostFunction();
       problem.AddResidualBlock(costFunction, nullptr, unknowns);
+      cout << constraint << endl;
     }
     ceres::Solver::Options options;
     options.linear_solver_type = ceres::DENSE_QR;
+    options.use_nonmonotonic_steps = true;
     options.minimizer_progress_to_stdout = true;
     ceres::Solver::Summary summary;
     Solve(options, &problem, &summary);
+    cout << summary.FullReport() << endl;
 
-    std::cout << summary.FullReport() << "\n";
-    /*Expression error = getErrorExpression();*/
-    /*error.print();*/
-    /*set<const Variable*> unknowns = error.getUnknowns();*/
-    /*vector<double> solution(unknowns.size());*/
-    /*for (unsigned i = 0; i < solution.size(); i++) {*/
-    /*  solution[i] = 0.0;*/
-    /*}*/
-    /*expressionMap* map = error.getMap();*/
-    /*for (auto entry : *map) {*/
-    /*  entry.first->value = solution[entry.second];*/
-    /*  entry.first->known = true;*/
-    /*}*/
-    /*delete map;*/
+    // Uncomment for debugging
+    /*std::cout << summary.FullReport() << "\n";*/
   }
 
 private:
