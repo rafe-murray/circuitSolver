d3e9cd594419a280a0c05104ee59be8a7d532c6a
rafe <rafemurr@gmail.com>
2025-08-09 23:11:02 -0400

    Refactor ExpressionNodes to use templated evaluate functions

---
+++ include/circuitsolver/expressionNode.h
@@ -0,0 +1,313 @@
+#ifndef EXPRESSIONNODE_H
+#define EXPRESSIONNODE_H
+
+#include <memory>
+#include <ostream>
+#include <unordered_map>
+#include <unordered_set>
+
+class VariableNode;
+class ExpressionNode;
+
+typedef std::unordered_map<const double*, size_t> ExpressionMap;
+typedef std::shared_ptr<ExpressionNode> ExpressionNodePtr;
+
+/**
+ * A single node in the AST of an `Expression`
+ */
+struct ExpressionNode {
+  /**
+   * virtual destructor to enable dynamic dispatch
+   */
+  virtual ~ExpressionNode() {}
+
+  /**
+   * Evaluates the AST with `this` as a root.
+   * Note that this is templated so that `ceres` can do automatic
+   * differentiation
+   * @param parameters an array of values to be used for the unknowns
+   * @param map a mapping from pointers to the unknown values to the index of
+   * the corresponding value to use in `parameters`
+   * @return the value of the AST with `this` as a root
+   */
+  template <typename T>
+  T evaluate(T const* parameters, const ExpressionMap& map) const;
+
+  /**
+   * Stores pointers to all unknown values in the AST with `this` as a root in
+   * `unknowns`
+   * @param unknowns a set of unknown values to add the unknowns in this AST to.
+   * Unknowns in this AST may or may not be already present already;
+   */
+  virtual void
+  getUnknowns(std::unordered_set<const double*>& unknowns) const = 0;
+
+  /**
+   * Prints the node to `out`
+   * @param out the stream to print to
+   * @return `out`
+   */
+  virtual std::ostream& operator<<(std::ostream& out) const = 0;
+};
+
+/**
+ * Types of binary operations
+ */
+enum class BinaryOp { MUL, DIV, ADD, SUB };
+
+/**
+ * A Binary operation node in the AST
+ */
+struct BinaryOpNode : ExpressionNode {
+
+  /**
+   * Creates a `BinaryOpNode`
+   * @param lhs the left hand side of the operation
+   * @param rhs the right hand side of the operation
+   * @param op the type of operation
+   */
+  BinaryOpNode(ExpressionNodePtr lhs, ExpressionNodePtr rhs, BinaryOp op);
+
+  /**
+   * Evaluates this node in the AST
+   * @param parameters an array of values to be used for the unknowns
+   * @param map a mapping from pointers to the unknown values to the index of
+   * the corresponding value to use in `parameters`
+   * @return the value of the AST with `this` as a root
+   */
+  template <typename T>
+  T evaluateImplementation(T const* parameters, const ExpressionMap& map) const;
+
+  /**
+   * @inheritdoc
+   */
+  void getUnknowns(std::unordered_set<const double*>& unknowns) const override;
+
+  /**
+   * @inheritdoc
+   */
+  std::ostream& operator<<(std::ostream& out) const override;
+
+  /**
+   * The left hand side of the operation
+   */
+  ExpressionNodePtr lhs;
+
+  /**
+   * The right hand side of the operation
+   */
+  ExpressionNodePtr rhs;
+
+  /**
+   * The type of operation
+   */
+  BinaryOp op;
+};
+
+/**
+ * Types of binary operations that return booleans
+ */
+enum class BooleanBinaryOp { LT, LEQ, EQ, NEQ, GEQ, GT };
+
+/**
+ * A Binary operation node in the AST that return a boolean value.
+ * Note that all other nodes return `double` values
+ */
+struct BooleanBinaryOpNode {
+
+  /**
+   * Creates a `BooleanBinaryOpNode`
+   * @param lhs the left hand side of the operation
+   * @param rhs the right hand side of the operation
+   * @param op the type of operation
+   */
+  BooleanBinaryOpNode(ExpressionNodePtr lhs, ExpressionNodePtr rhs,
+                      BooleanBinaryOp op);
+
+  /**
+   * Evaluates the AST with `this` as a root.
+   * Note that this is templated so that `ceres` can do automatic
+   * differentiation
+   * @param parameters an array of values to be used for the unknowns
+   * @param map a mapping from pointers to the unknown values to the index of
+   * the corresponding value to use in `parameters`
+   * @return the value of the AST with `this` as a root
+   */
+  template <typename T>
+  bool evaluate(T const* parameters, const ExpressionMap& map) const;
+
+  /**
+   * Stores pointers to all unknown values in the AST with `this` as a root in
+   * `unknowns`
+   * @param unknowns a set of unknown values to add the unknowns in this AST to.
+   * Unknowns in this AST may or may not be already present already;
+   */
+  void getUnknowns(std::unordered_set<const double*>& unknowns) const;
+
+  /**
+   * Prints the node to `out`
+   * @param out the stream to print to
+   * @return `out`
+   */
+  std::ostream& operator<<(std::ostream& out) const;
+
+  /**
+   * The left hand side of the operation
+   */
+  ExpressionNodePtr lhs;
+
+  /**
+   * The right hand side of the operation
+   */
+  ExpressionNodePtr rhs;
+
+  /**
+   * The type of operation
+   */
+  BooleanBinaryOp op;
+};
+
+/**
+ * A Ternary operation node in the AST
+ */
+struct TernaryOpNode : ExpressionNode {
+  /*
+   * Creates a `TernaryOpNode`
+   * @param condition what to test to determine which expression to use
+   * @param valIfTrue expression to use if `condition` is true
+   * @param valIfFalse expression to use if `condition` is false
+   */
+  TernaryOpNode(std::shared_ptr<BooleanBinaryOpNode> condition,
+                ExpressionNodePtr valIfTrue, ExpressionNodePtr valIfFalse);
+
+  /**
+   * Evaluates this node in the AST
+   * @param parameters an array of values to be used for the unknowns
+   * @param map a mapping from pointers to the unknown values to the index of
+   * the corresponding value to use in `parameters`
+   * @return the value of the AST with `this` as a root
+   */
+  template <typename T>
+  T evaluateImplementation(T const* parameters, const ExpressionMap& map) const;
+
+  /**
+   * @inheritdoc
+   */
+  void getUnknowns(std::unordered_set<const double*>& unknowns) const override;
+
+  /**
+   * @inheritdoc
+   */
+  std::ostream& operator<<(std::ostream& out) const override;
+
+  /**
+   * The condition used to determine which expression to evaluate
+   */
+  std::shared_ptr<BooleanBinaryOpNode> condition;
+
+  /**
+   * The expression to evaluate if `condition` is true
+   */
+  ExpressionNodePtr valIfTrue;
+
+  /**
+   * The expression to evaluate if `condition` is false
+   */
+  ExpressionNodePtr valIfFalse;
+};
+
+/**
+ * Types of unary operations
+ */
+enum class UnaryOp { EXP, NEG };
+
+/**
+ * A Unary operation node in the AST
+ */
+struct UnaryOpNode : ExpressionNode {
+
+  /**
+   * Creates a `UnaryOpNode`
+   * @param operand the operand for the operation
+   * @param op the type of operation
+   */
+  UnaryOpNode(ExpressionNodePtr operand, UnaryOp op);
+
+  /**
+   * Evaluates this node in the AST
+   * @param parameters an array of values to be used for the unknowns
+   * @param map a mapping from pointers to the unknown values to the index of
+   * the corresponding value to use in `parameters`
+   * @return the value of the AST with `this` as a root
+   */
+  template <typename T>
+  T evaluateImplementation(T const* parameters, const ExpressionMap& map) const;
+
+  /**
+   * @inheritdoc
+   */
+  void getUnknowns(std::unordered_set<const double*>& unknowns) const override;
+
+  /**
+   * @inheritdoc
+   */
+  std::ostream& operator<<(std::ostream& out) const override;
+
+  /**
+   * The operand for the operation
+   */
+  ExpressionNodePtr operand;
+
+  /**
+   * The type of operation
+   */
+  UnaryOp op;
+};
+
+/**
+ * A node representing a single known or unknown value in the AST
+ */
+struct VariableNode : ExpressionNode {
+  /**
+   * Creates a `VariableNode` representing an unknown
+   */
+  VariableNode();
+
+  /**
+   * Creates a `VariableNode` representing a known value
+   * @param value the value of the node
+   */
+  VariableNode(double value);
+
+  /**
+   * Evaluates this node in the AST
+   * @param parameters an array of values to be used for the unknowns
+   * @param map a mapping from pointers to the unknown values to the index of
+   * the corresponding value to use in `parameters`
+   * @return the value of the AST with `this` as a root
+   */
+  template <typename T>
+  T evaluateImplementation(T const* parameters, const ExpressionMap& map) const;
+
+  /**
+   * @inheritdoc
+   */
+  void getUnknowns(std::unordered_set<const double*>& unknowns) const override;
+
+  /**
+   * @inheritdoc
+   */
+  std::ostream& operator<<(std::ostream& out) const override;
+
+  /**
+   * The value of this node
+   */
+  double value;
+
+  /**
+   * Is the value known or does this node represent an unknown
+   */
+  bool known;
+};
+
+#endif
---
+++ src/expressionNode.cpp
@@ -0,0 +1,204 @@
+#include "circuitsolver/expressionNode.h"
+#include <cmath>
+
+BinaryOpNode::BinaryOpNode(ExpressionNodePtr lhs, ExpressionNodePtr rhs,
+                           BinaryOp op)
+    : lhs(lhs), rhs(rhs), op(op) {}
+
+BooleanBinaryOpNode::BooleanBinaryOpNode(ExpressionNodePtr lhs,
+                                         ExpressionNodePtr rhs,
+                                         BooleanBinaryOp op)
+    : lhs(lhs), rhs(rhs), op(op) {}
+
+TernaryOpNode::TernaryOpNode(std::shared_ptr<BooleanBinaryOpNode> condition,
+                             ExpressionNodePtr valIfTrue,
+                             ExpressionNodePtr valIfFalse)
+    : condition(condition), valIfTrue(valIfTrue), valIfFalse(valIfFalse) {}
+UnaryOpNode::UnaryOpNode(ExpressionNodePtr operand, UnaryOp op)
+    : operand(operand), op(op) {}
+
+VariableNode::VariableNode() : value(), known(false) {}
+VariableNode::VariableNode(double value) : value(value), known(true) {}
+
+template <typename T>
+T ExpressionNode::evaluate(T const* parameters,
+                           const ExpressionMap& map) const {
+  if (auto node = dynamic_cast<const VariableNode*>(this)) {
+    node->evaluateImplementation(parameters, map);
+  }
+  if (auto node = dynamic_cast<const BinaryOpNode*>(this)) {
+    node->evaluateImplementation(parameters, map);
+  }
+  if (auto node = dynamic_cast<const UnaryOpNode*>(this)) {
+    node->evaluate(parameters, map);
+  }
+  if (auto node = dynamic_cast<const TernaryOpNode*>(this)) {
+    node->evaluate(parameters, map);
+  }
+}
+
+template <typename T>
+T BinaryOpNode::evaluateImplementation(T const* parameters,
+                                       const ExpressionMap& map) const {
+  switch (op) {
+  case BinaryOp::MUL:
+    return lhs->evaluate(parameters, map) * rhs->evaluate(parameters, map);
+  case BinaryOp::DIV:
+    return lhs->evaluate(parameters, map) / rhs->evaluate(parameters, map);
+  case BinaryOp::ADD:
+    return lhs->evaluate(parameters, map) + rhs->evaluate(parameters, map);
+  case BinaryOp::SUB:
+    return lhs->evaluate(parameters, map) - rhs->evaluate(parameters, map);
+  }
+}
+
+template <typename T>
+bool BooleanBinaryOpNode::evaluate(T const* parameters,
+                                   const ExpressionMap& map) const {
+  switch (op) {
+  case BooleanBinaryOp::EQ:
+    return lhs->evaluate(parameters, map) == rhs->evaluate(parameters, map);
+  case BooleanBinaryOp::GEQ:
+    return lhs->evaluate(parameters, map) >= rhs->evaluate(parameters, map);
+  case BooleanBinaryOp::LEQ:
+    return lhs->evaluate(parameters, map) <= rhs->evaluate(parameters, map);
+  case BooleanBinaryOp::LT:
+    return lhs->evaluate(parameters, map) < rhs->evaluate(parameters, map);
+  case BooleanBinaryOp::GT:
+    return lhs->evaluate(parameters, map) > rhs->evaluate(parameters, map);
+  case BooleanBinaryOp::NEQ:
+    return lhs->evaluate(parameters, map) != rhs->evaluate(parameters, map);
+  }
+}
+
+template <typename T>
+T TernaryOpNode::evaluateImplementation(T const* parameters,
+                                        const ExpressionMap& map) const {
+  if (condition->evaluate(parameters, map)) {
+    return valIfTrue->evaluate(parameters, map);
+  } else {
+    return valIfFalse->evaluate(parameters, map);
+  }
+}
+
+template <typename T>
+T UnaryOpNode::evaluateImplementation(T const* parameters,
+                                      const ExpressionMap& map) const {
+  switch (op) {
+  case UnaryOp::EXP:
+    return std::exp(operand->evaluate(parameters, map));
+  case UnaryOp::NEG:
+    return -(operand->evaluate(parameters, map));
+  }
+}
+
+template <typename T>
+T VariableNode::evaluateImplementation(T const* parameters,
+                                       const ExpressionMap& map) const {
+  if (known) {
+    return T(value);
+  } else {
+    return parameters[map.at(&value)];
+  }
+}
+
+void BinaryOpNode::getUnknowns(
+    std::unordered_set<const double*>& unknowns) const {
+  lhs->getUnknowns(unknowns);
+  rhs->getUnknowns(unknowns);
+}
+
+void BooleanBinaryOpNode::getUnknowns(
+    std::unordered_set<const double*>& unknowns) const {
+  lhs->getUnknowns(unknowns);
+  rhs->getUnknowns(unknowns);
+}
+
+void TernaryOpNode::getUnknowns(
+    std::unordered_set<const double*>& unknowns) const {
+  condition->getUnknowns(unknowns);
+}
+
+void UnaryOpNode::getUnknowns(
+    std::unordered_set<const double*>& unknowns) const {
+  operand->getUnknowns(unknowns);
+}
+
+void VariableNode::getUnknowns(
+    std::unordered_set<const double*>& unknowns) const {
+  unknowns.insert(&value);
+}
+
+std::ostream& BinaryOpNode::operator<<(std::ostream& out) const {
+  out << "(" << lhs;
+  switch (op) {
+  case BinaryOp::MUL:
+    out << " * ";
+    break;
+  case BinaryOp::DIV:
+    out << " / ";
+    break;
+  case BinaryOp::ADD:
+    out << " + ";
+    break;
+  case BinaryOp::SUB:
+    out << " - ";
+    break;
+  }
+  out << ")";
+  return out;
+}
+
+std::ostream& BooleanBinaryOpNode::operator<<(std::ostream& out) const {
+  out << "(" << lhs;
+  switch (op) {
+  case BooleanBinaryOp::EQ:
+    out << "==";
+    break;
+  case BooleanBinaryOp::GEQ:
+    out << ">=";
+    break;
+  case BooleanBinaryOp::LEQ:
+    out << "<=";
+    break;
+  case BooleanBinaryOp::GT:
+    out << ">";
+    break;
+  case BooleanBinaryOp::LT:
+    out << "<";
+    break;
+  case BooleanBinaryOp::NEQ:
+    out << "!=";
+    break;
+  }
+  out << ")";
+  return out;
+}
+
+std::ostream& TernaryOpNode::operator<<(std::ostream& out) const {
+  out << "(" << condition << " ? " << valIfTrue << " : " << valIfFalse << ")";
+  return out;
+}
+
+std::ostream& UnaryOpNode::operator<<(std::ostream& out) const {
+  out << "(";
+  switch (op) {
+  case UnaryOp::EXP:
+    out << "e^";
+    break;
+  case UnaryOp::NEG:
+    out << "-";
+    break;
+  }
+  out << operand;
+  return out;
+}
+
+std::ostream& VariableNode::operator<<(std::ostream& out) const {
+  if (known) {
+    out << value;
+  } else {
+    out << this;
+  }
+  return out;
+}
